96ae6d0e7254aab2515e92acfd531d5896552b78
Vytenis Silgalis <vytenis.silgalis@riseinteractive.com>
2016-10-31 15:49:51 -0500

    freetds-dev added

--- spark16/workerInstall.sh
+++ spark16/workerInstall.sh
@@ -25,7 +25,7 @@ pip_install()
 apt_install()
 {
     apt-get update
-    apt-get install htop vim-nox libjpeg-dev zlib1g-dev libffi-dev g++ librdkafka-dev build-essential libssl-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk -y
+    apt-get install htop vim-nox libjpeg-dev zlib1g-dev libffi-dev g++ librdkafka-dev build-essential libssl-dev python-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk freetds-dev -y
 }
 
 set_env()
